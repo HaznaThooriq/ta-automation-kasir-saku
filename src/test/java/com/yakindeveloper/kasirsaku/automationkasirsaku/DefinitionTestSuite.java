409fee09deb72b79b5f566390eefa12cbefa664b
DESKTOP-FG4TJP0\Hzn <haznaatthooriqoh@gmail.com>
2018-11-21 12:56:51 +0700

    Pelanggan dan Supplier

--- src/test/java/com/yakindeveloper/kasirsaku/automationkasirsaku/DefinitionTestSuite.java
+++ src/test/java/com/yakindeveloper/kasirsaku/automationkasirsaku/DefinitionTestSuite.java
@@ -5,5 +5,5 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/features/APIKasirsaku/APIHutang.feature", tags = "")
+@CucumberOptions(features="src/test/resources/features/APIKasirsaku/APIPelanggan.feature", tags = "")
 public class DefinitionTestSuite {}
