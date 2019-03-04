49f64e0cd19c785fc27d0f93f041e4ff2fa5cb7f
haznathooriq <haznaatthooriqoh@gmail.com>
2019-03-04 10:59:48 +0700

    set timeout by misbah

--- src/test/java/com/yakindeveloper/kasirsaku/automationkasirsaku/DefinitionTestSuite.java
+++ src/test/java/com/yakindeveloper/kasirsaku/automationkasirsaku/DefinitionTestSuite.java
@@ -3,7 +3,14 @@ package com.yakindeveloper.kasirsaku.automationkasirsaku;
 import cucumber.api.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features="src/test/resources/features/WebKasirSaku/Login.feature", tags = "@Login")
-public class DefinitionTestSuite {}
+public class DefinitionTestSuite {
+}
